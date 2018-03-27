0f360abf1b1e871479c174c8944bdfe90de72656
Carlos Brito Lage <carlos@carloslage.net>
2018-03-27 15:09:38 +0100

    woot

--- mix.exs
+++ mix.exs
@@ -2,15 +2,20 @@ defmodule BenchCrunch_010_SNAPSHOT.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bench_crunch, version: "0.1.0-SNAPSHOT", elixir: "~> 1.6", deps: deps(Mix.env())]
+    elixirc_defaults = [debug_info: true, ignore_module_conflict: true, docs: true]
+    [ app: :bench_crunch,
+      version: "0.1.0-SNAPSHOT",
+      elixir: "~> 1.6",
+      elixirc_options: elixirc_defaults ++ options(Mix.env()),
+      deps: deps(Mix.env())]
   end
+
+  defp options(env) when env in [:dev, :test], do: [exlager_level: :debug, exlager_truncation_size: 8096]
+  defp options(_), do: []
 
   def application, do: []
 
-  defp inch_ex,
-    do:
-      {:inch_ex, github: "cblage/inch_ex", branch: "master", only: [:dev, :test], runtime: false}
-
+  defp inch_ex, do: [{:inch_ex, github: "cblage/inch_ex", branch: "master", only: [:dev, :test], runtime: false}]
   defp ex_doc, do: {:ex_doc, "~> 0.16", only: :dev, runtime: false}
   defp json(:dev), do: {:json, github: "cblage/elixir-json", branch: "develop", override: true}
   defp json(:prod), do: {:json, "~> 1.0"}
@@ -18,9 +23,7 @@ defmodule BenchCrunch_010_SNAPSHOT.Mixfile do
   defp poison(:prod), do: {:poison, "~> 3.1"}
   defp jason(:dev), do: {:jason, github: "michalmuskala/jason", branch: "master", override: true}
   defp jason(:prod), do: {:jason, "~> 1.0"}
-
-  defp credo,
-    do: {:credo, github: "cblage/credo", branch: "master", only: [:dev, :test], runtime: false}
+  defp credo, do: {:credo, github: "cblage/credo", branch: "master", only: [:dev, :test], runtime: false}
 
   def deps(:test), do: [ex_doc(), inch_ex(), json(:dev), poison(:dev), jason(:dev)]
   def deps(:docs), do: [ex_doc(), inch_ex(), credo()]
---
+++ package.exs
@@ -0,0 +1,9 @@
+Expm.Package.new(
+  name: "json",
+  description: "Native Elixir library for JSON encoding and decoding",
+  version: "2.0.0-SNAPSHOT",
+  keywords: ["Elixir", "json", "encoder", "decoder", "encode", "decode", "native"],
+  licenses: [[name: "BSD 3-Clause"]],
+  maintainers: [
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]],
+  repositories: [[github: "cblage/elixir-json", branch: "develop"]])
